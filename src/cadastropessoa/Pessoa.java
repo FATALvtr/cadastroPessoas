7dc9df303c364d980320d77820e7437825360143
Rafeixons <goncalvesrafeixons@gmail.com>
2026-04-09 19:21:02 -0300

    Formatacao do CPF

--- src/cadastropessoa/Pessoa.java
+++ src/cadastropessoa/Pessoa.java
@@ -1,19 +1,17 @@
-
 package cadastropessoa;
 
-
 public class Pessoa {
-    
+
     String nome;
     int idade;
-    String CPF;
+    String cpf;
 
     public Pessoa(String nome, int idade, String CPF) {
         this.nome = nome;
         this.idade = idade;
-        this.CPF = CPF;
+        this.cpf = CPF;
     }
-    
+
     // construtor vazio pro Scanner
     public Pessoa() {
     }
@@ -34,16 +32,19 @@ public class Pessoa {
         this.idade = idade;
     }
 
-    public String getCPF() {
-        return CPF;
+    public String getCPF(String a) {
+
+        if (a == null || a.length() != 11) {
+            return cpf;
+        }
+
+        return a.replaceAll("(\\d{3})(\\d{3})(\\d{3})(\\d{2})",
+                "$1.$2.$3-$4");
+
     }
 
     public void setCPF(String CPF) {
-        this.CPF = CPF;
+        this.cpf = CPF;
     }
-    
-    
-    
-    
 
 }
