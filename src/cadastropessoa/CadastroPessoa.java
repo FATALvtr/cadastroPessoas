13178c9ed08d506f0ad193ec2f880c427e576421
FATALvtr <vitordias1502@gmail.com>
2026-04-10 01:00:31 -0300

    CadastroPessoa Finalizado

--- src/cadastropessoa/CadastroPessoa.java
+++ src/cadastropessoa/CadastroPessoa.java
@@ -5,62 +5,58 @@ import java.util.ArrayList;
 
 
 public class CadastroPessoa {
+    ArrayList <Pessoa> lista = new ArrayList();
     
-    public void cadastrarPessoas(){
-        Scanner s = new Scanner(System.in);
-        Pessoa cadastrar = new Pessoa();
-        
-        int decisao = 0;
-        
-        do {
-            System.out.print("\nDigite o nome: ");
-            cadastrar.setNome(s.next());
-
-            System.out.print("\nDigite o CPF: ");
-            cadastrar.setCPF(s.next());
-            
-            System.out.print("\nDigite a idade: ");
-            cadastrar.setIdade(s.nextInt());
-            
-            cadastrar.imprimir();
-                    
-            System.out.println("\nDigite 1 para fazer novo cadastro / Digite 0 para voltar ao menu!");
-            decisao = s.nextInt();
-                
-        } while (decisao != 0);
-        
-        
-    }
-    public void listarPessoas(Pessoa pessoa1){
-        
-        ArrayList <Pessoa> lista = new ArrayList();
-        
-        lista.add(pessoa1);
-        
-    }
-    public void menu(Pessoa pessoa1){
+    public void menu(){
         Scanner scanner = new Scanner(System.in);
+        int escolha = 0;
+        //Faça-enquanto.
+        do {            
+            System.out.print("\n1 - Cadastrar \n2 - Listar Pessoas\n0 - Sair");
+            System.out.print("\nEscolha: ");
+            escolha = scanner.nextInt();
+//OPÇÕES - switch case com -> / pra ficar mais simples
+            switch (escolha) {
+                case 1-> cadastrarPessoas();
+                case 2-> listarPessoas(); 
+                case 0-> System.out.println("Obrigado por usar o serviço!"); 
+                default-> System.out.print("Opção: "+escolha+" não é valida!");
+            }
+        }while (escolha != 0);
+    }
+    public void cadastrarPessoas(){
         
-        System.out.print("1 - cadastrar");
-        System.out.print("\n2- listar pessoas");
-        System.out.print("\n3- Sair\n");
-        int escolha = scanner.nextInt();
+        Scanner s = new Scanner(System.in);
         
-        switch (escolha) {
-            case 1:
-                cadastrarPessoas();
-                break;
-            case 2: 
-                listarPessoas(pessoa1);
-                break;
-            case 3: 
-                System.out.println("Obrigado por usar o serviço!");
-                break;
-            default:
-                System.out.print("Escolha uma opção valida!");
+        Pessoa nova = new Pessoa();
+        
+        System.out.print("\nDigite o nome: ");
+        nova.setNome(s.nextLine());
+
+        System.out.print("\nDigite o CPF: ");
+        nova.setCPF(s.next());
+
+        System.out.print("\nDigite a idade: ");
+        nova.setIdade(s.nextInt());
+        s.nextLine(); //serve para tirar o (limpeza de buffer/pulo do gato)
+                
+        lista.add(nova);//função adiciona novo objeto a lista
+        
+        System.out.println("\n"+nova.getNome()+" Cadastrado com sucesso!");
+        
+        //nova.imprimir();
+    }
+    public void listarPessoas(){
+        if(lista.isEmpty()){//função indentifica vazio
+            System.out.println("\nLista vazia!");
+        }else{
+            for (Pessoa p : lista){
+                System.out.println("");
+                p.imprimir();
+            }
         }
     }
 
-    
-
 }
+
+
--- src/cadastropessoa/main.java
+++ src/cadastropessoa/main.java
@@ -1,19 +1,10 @@
 
 package cadastropessoa;
 
-import java.util.ArrayList;
-
-
 public class main {
     public static void main(String[] args) {
-        
-        Pessoa pessoa1 = new Pessoa();
-        
         CadastroPessoa c = new CadastroPessoa();
-        
-        ArrayList lista = new ArrayList();
-        
-        c.menu(pessoa1);
+        c.menu();
         
     }
 
