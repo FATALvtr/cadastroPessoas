70e114d60d9921a2d0cd43701a60236136b1e6aa
FATALvtr <vitordias1502@gmail.com>
2026-04-09 21:38:04 -0300

    Main e funções de cadastro/listar

--- src/cadastropessoa/CadastroPessoa.java
+++ src/cadastropessoa/CadastroPessoa.java
@@ -1,13 +1,61 @@
 
 package cadastropessoa;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 
 public class CadastroPessoa {
-
-
-    public static void main(String[] args) {
+    
+    public void cadastrarPessoas(){
+        Scanner s = new Scanner(System.in);
+        Pessoa cadastrar = new Pessoa();
         
+        int decisao = 0;
+        
+        do {
+            System.out.print("\nDigite o nome: ");
+            cadastrar.setNome(s.next());
 
+            System.out.print("\nDigite o CPF: ");
+            cadastrar.setCPF(s.next());
+            
+            System.out.print("\nDigite a idade: ");
+            cadastrar.setIdade(s.nextInt());
+            
+            cadastrar.imprimir();
+                    
+            System.out.println("\nDigite 1 para sair / d1igite 0 para fazer novo cadastro!");
+            decisao = s.nextInt();
+                
+        } while (decisao != 1);
     }
+    public void listarPessoas(){
+        ArrayList <Pessoa> lista = new ArrayList();
+        
+        System.out.print("Digite o nome que deseja consultar: ");
+        
+        
+        
+    }
+    public void menu(){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("1 - cadastrar");
+        System.out.println("2- listar pessoas");
+        int escolha = scanner.nextInt();
+        
+        switch (escolha) {
+            case 1:
+                cadastrarPessoas();
+                break;
+            case 2: 
+                listarPessoas();
+                break;
+            default:
+                System.out.print("Escolha uma opção valida!");
+        }
+    }
+
+    
 
 }
---
+++ src/cadastropessoa/main.java
@@ -0,0 +1,19 @@
+
+package cadastropessoa;
+
+import java.util.ArrayList;
+
+
+public class main {
+    public static void main(String[] args) {
+        
+        Pessoa pessoa1 = new Pessoa();
+        
+        CadastroPessoa c = new CadastroPessoa();
+        
+        ArrayList lista = new ArrayList();
+        
+        c.menu();
+    }
+
+}
